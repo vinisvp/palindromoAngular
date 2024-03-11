de607159e8fb8ad82c85117e367de7ed7df8ba38
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-03-10 23:56:19 -0300

    Added the unknown image, and the palindrome ignores the letters case and accent

--- src/app/palindrome/palindrome.component.ts
+++ src/app/palindrome/palindrome.component.ts
@@ -16,25 +16,32 @@ export class PalindromeComponent {
 
   checkPalind()
   {
-    this.text = this.text.toUpperCase();
-    this.auxText = '';
-    this.array = this.text.split('');
-    for(let i = this.array.length - 1; i >= 0; i--)
+    if (this.text.trim() != '' && this.text.trim() != ' ')
     {
-      this.auxText = this.auxText.concat(this.array[i].toString());
-    }
-    console.log(this.text + ' ' + this.auxText);
-    if (this.text == this.auxText)
-    {
-      console.log('É um palindromo!');
-      this.img = '../assets/img/yes.png';
-      this.message = 'É um palindromo!';
+      this.auxText = '';
+      this.array = this.text.split('');
+      for(let i = this.array.length - 1; i >= 0; i--)
+      {
+        this.auxText = this.auxText.concat(this.array[i].toString().normalize('NFD').replace(/[\u0300-\u036f]/g, ''));
+      }
+      console.log(this.text + '|' + this.auxText);
+      if (this.text.trim().toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '') == this.auxText.trim().toUpperCase())
+      {
+        console.log('É um palindromo!');
+        this.img = '../assets/img/yes.png';
+        this.message = 'É um palindromo!';
+      }
+      else
+      {
+        console.log('Não é um palindromo!');
+        this.img = '../assets/img/no.png';
+        this.message = 'Não é um palindromo!';
+      }
     }
     else
     {
-      console.log('Não é um palindromo!');
-      this.img = '../assets/img/no.png';
-      this.message = 'Não é um palindromo!';
+      this.img = '../assets/img/unknown.png';
+      this.message = 'É necessário uma palavra para prosseguir!';
     }
   }
 }
